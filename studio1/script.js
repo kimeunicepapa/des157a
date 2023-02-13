1b8de0d154825798017e61bd427f75a9e0183ca6
Kim Eunice Papa <papakimeunice@gmail.com>
2023-02-13 03:33:46 -0800

    madlibs2

--- studio1/script.js
+++ studio1/script.js
@@ -4,6 +4,7 @@
 
     const myForm = document.querySelector('#myForm');
     const madLibs = document.querySelector('#madLibs');
+    // const error = document.querySelector('#error');
 
     myForm.addEventListener('submit', function(event){
 
@@ -17,44 +18,48 @@
         const colorDay = document.querySelector('#colorDay').value;
 
         let myText;
+        // let errorMessage;
 
-        if (badHabit == ''){
-            myText = "Please tell me your bad habit, no one is judging";
-            document.querySelector('#badHabit').focus();
-        } else if (adjective == ''){
-            myText = "Please tell me an adjective, no one is judging";
-            document.querySelector('#adjective').focus();
-        } else if (favActivity == ''){
-            myText = "Please tell me what you love to do, no one is judging";
-            document.querySelector('#favActivity').focus();
-        } else if (emotion == ''){
-            myText = "Please tell me how you feel, no one is judging";
-            document.querySelector('#emotion').focus();
-        } else if (list == ''){
-            myText = "Please tell me how you feel, no one is judging";
-            document.querySelector('#list').focus();
-        } else if (colorDay== ''){
-            myText = "Please tell me how you feel, no one is judging";
-            document.querySelector('#colorDay').focus();
-        } else {
+        // if (badHabit == ''){
+        //     myText = "Please tell me your bad habit, no one is judging";
+        //     document.querySelector('#badHabit').focus();
+        // } else if (adjective == ''){
+        //     myText = "Please tell me an adjective, no one is judging";
+        //     document.querySelector('#adjective').focus();
+        // } else if (favActivity == ''){
+        //     myText = "Please tell me what you love to do, no one is judging";
+        //     document.querySelector('#favActivity').focus();
+        // } else if (emotion == ''){
+        //     myText = "Please tell me how you feel, no one is judging";
+        //     document.querySelector('#emotion').focus();
+        // } else if (list == ''){
+        //     myText = "Please tell me what you love, no one is judging";
+        //     document.querySelector('#list').focus();
+        // } else {
             myText = `I will focus on shifting my pattern of ${badHabit}.
             Who I’m becoming will be ${adjective}.
             I feel like I can be my best self when I ${favActivity}.
             When I think about my future self, I feel ${emotion}.
             ${list} are the three words I want to keep in mind all day.`;
-            // document.querySelector('#container img').style.backgroundColor = '#colorDay';
+
+
+            document.querySelector('#container img').style.backgroundColor = colorDay;
 
             document.querySelector('#badHabit').value = '';
             document.querySelector('#adjective').value = '';
             document.querySelector('#favActivity').value = '';
             document.querySelector('#emotion').value = '';
             document.querySelector('#list').value = '';
-            document.querySelector('#colorDay').value = '';
-        }
+            document.querySelector('#colorDay').value = '#8BB174';
+        // }
 
         // document.querySelector('#container img').style.backgroundColor = 'blue';
 
         madLibs.innerHTML = myText;
+
+        document.getElementById ('#output').className= 'showing';
+        document.getElementById ('#input').className= 'hidden';
+
 
     });
     // document.querySelector('#container img').style.backgroundColor = 'blue';
